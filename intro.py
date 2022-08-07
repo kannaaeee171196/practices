a1ab88ffe15be40a06b20433de28095c0904cbf9
kannaaeee171196 <110761637+kannaaeee171196@users.noreply.github.com>
2022-08-07 13:34:58 +0530

    Add files via upload
    intro

---
+++ intro.py
@@ -0,0 +1,7 @@
+dictionary={"name ":"kannan","age":"25"}
+word=input("enter the word:")
+def check(d):
+    return dictionary[d]
+print(check(word))
+liabury import json
+json file=json.1load(open("data1.json"))
